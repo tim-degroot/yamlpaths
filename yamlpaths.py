054e11d572b45d63d2abeae5d1569de97ceb830a
tim-degroot <timdegroot345@gmail.com>
2024-11-14 14:50:22 +0100

    black reformatting

--- yamlpaths.py
+++ yamlpaths.py
@@ -5,42 +5,49 @@ import yaml
 import reaction_pypaths
 import argparse
 
-def plot(file, normalize=None, folder=''):
+
+def plot(file, normalize=None, folder=""):
     values, order = get_values(file, normalize=normalize)
     for key, sub_order in order.items():
-        yaml_path(values, list(sub_order.keys()), f'{key}.png', folder)
+        yaml_path(values, list(sub_order.keys()), f"{key}.png", folder)
 
-def gauss_get_energy(filepath, energy='Sum of electronic and zero-point Energies'):
-    HARTREE_TO_EV = scipy.constants.physical_constants['Hartree energy in eV'][0]
+
+def gauss_get_energy(filepath, energy="Sum of electronic and zero-point Energies"):
+    HARTREE_TO_EV = scipy.constants.physical_constants["Hartree energy in eV"][0]
     with open(filepath) as f:
         for line in f:
             if energy in line:
                 number = float(line.split()[-1])
                 return number * HARTREE_TO_EV
 
+
 def get_values(file, normalize):
     with open(file) as f:
         data = yaml.load(f, Loader=yaml.CLoader)
 
-    file_location = data['File location']
-    ts_location = data['TS location']
-    file_prefix = data['File prefix']
-    file_suffix = data['File suffix']
-    order = data['Diagrams']
+    file_location = data["File location"]
+    ts_location = data["TS location"]
+    file_prefix = data["File prefix"]
+    file_suffix = data["File suffix"]
+    order = data["Diagrams"]
     values = {}
 
     for sub_order in order.values():
         for key, value in sub_order.items():
-            full_path = f''
-            full_path += ts_location if 'to' in key else file_location
+            full_path = f""
+            full_path += ts_location if "to" in key else file_location
             full_path += file_prefix
             full_path += key if value is None else value
             full_path += file_suffix
-            
+
             values[key] = gauss_get_energy(full_path)
 
-    normalized_values = {key: round((value - values.get(normalize, min(values.values()))), 2) for key, value in values.items()}
+    normalized_values = {
+        key: round((value - values.get(normalize, min(values.values()))), 2)
+        for key, value in values.items()
+    }
     return normalized_values, order
+
 
 def yaml_path(values, order, filename, folder):
     energy_diagram = reaction_pypaths.Diagram()
@@ -54,15 +61,24 @@ def yaml_path(values, order, filename, folder):
         energy_diagram.add_link(levels[order[i]], levels[order[i + 1]])
 
     # Plot the diagram and save the final result to the file
-    energy_diagram.plot(f'{folder}{filename}')
+    energy_diagram.plot(f"{folder}{filename}")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Generate reaction path energy diagrams from YAML')
-    parser.add_argument('yaml_file', type=str, help='The path to the YAML file')
-    parser.add_argument('-n', '--normalize', type=str, help='The key to normalize the values', default=None)
-    parser.add_argument('-o', '--output', type=str, help='Output directory', default='')
+    parser = argparse.ArgumentParser(
+        description="Generate reaction path energy diagrams from YAML"
+    )
+    parser.add_argument("yaml_file", type=str, help="The path to the YAML file")
+    parser.add_argument(
+        "-n",
+        "--normalize",
+        type=str,
+        help="The key to normalize the values",
+        default=None,
+    )
+    parser.add_argument("-o", "--output", type=str, help="Output directory", default="")
     args = parser.parse_args()
 
     values, order = get_values(args.yaml_file, normalize=args.normalize)
     for key, sub_order in order.items():
-        yaml_path(values, list(sub_order.keys()), f'{key}.png', args.o)
+        yaml_path(values, list(sub_order.keys()), f"{key}.png", args.o)
